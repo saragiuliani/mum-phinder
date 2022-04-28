bd97d757976cc15632efae76f84099ef8b364a6b
saragiuliani <sgiuliani94@gmail.com>
2022-04-28 14:24:19 +0200

    add script for data example

--- experiments/sars-cov2/download.sh
+++ experiments/sars-cov2/download.sh
@@ -1,21 +1,27 @@
 
+# to download the reference sequences
+
 wget https://ftp.ebi.ac.uk/pub/databases/covid19dataportal/cdp-file-downloader.zip
 unzip cdp-file-downloader.zip
 
 mkdir -p ${1}
+#mkdir -p ${1}/Ref
+#mkdir -p ${1}/Pattern
 
-for i in {00..39};
+for i in {00..01};
 do
     acc="accessions/acc_${i}"
     java -jar cdp-file-downloader.jar --domain=VIRAL_SEQUENCES --datatype=SEQUENCES --format=FASTA --location=${1} --email=NONE --accessions=`awk '{gsub("\"",""); print $1}' ${acc} | paste -s -d, -` --protocol=FTP --asperaLocation=null
 done
 
-touch ${1}/SARS-CoV2.400k.fa
+touch ${1}/SARS-CoV2.2k.fa
 files=(`ls ${1}/viral_sequences/sequences/fasta/`)
 for i in ${files[@]};
 do
-    cat ${1}/viral_sequences/sequences/fasta/${i} >> ${1}/SARS-CoV2.400k.fa
+    cat ${1}/viral_sequences/sequences/fasta/${i} >> ${1}/SARS-CoV2.2k.fa
     rm ${1}/viral_sequences/sequences/fasta/${i}
 done
 
+# to download the query sequence
 wget -O ${1}/MZ477765.fa https://www.ebi.ac.uk/ena/browser/api/fasta/MZ477765.1?download=true
+
